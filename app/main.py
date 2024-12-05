88d519ad66499d5ea8facf576329ece3f83bea5f
root <root@DESKTOP-J1P7D3U>
2024-12-05 18:40:39 -0500

    completed step 4

--- app/main.py
+++ app/main.py
@@ -1,25 +1,82 @@
 #!/usr/bin/env python3
-
+import mysql.connector
+from mysql.connector import Error
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import MySQLdb
+# from typing import Optional
+# from pydantic import BaseModel
+from chalice import Chalice
+import boto3
+
+app = Chalice(app_name="main.py")
+app.debug = True
+
+S3_BUCKET = 'dpv8cf-dp1-spotify'
+s3=boto3.client('s3')
+
+DBUSER = os.getenv('DBUSER')
+DBHOST = os.getenv('DBHOST')
+DBPASS = os.getenv('DBPASS')
+DB = "dpv8cf"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Today is": "Saturday!"}
+    return {"Good Day": "Sunshine!"}
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+@app.get('/genres')
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        # cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        # cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
 
-@app.get("/square/{c}")
-def square(c: int):
-    return {"square": c * c}
-
-@app.get("/multiply/{d}/{e}")
-def multiply(d: int, e: int):
-    return {"multiply": d * e}
+@app.get('/songs')
+async def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
+    cur = db.cursor()
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try:
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return None
